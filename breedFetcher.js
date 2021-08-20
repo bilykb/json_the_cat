42584f2e1f4184c6f30b3b35965b5d52e8bfdca0
bilykb <brettbilyk@gmail.com>
2021-08-20 16:41:33 +0000

    ran through eslint

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,15 +3,17 @@ const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
   request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, function(error, response, body) {
-    if (error) {
-      callback(error, null);
-    } else {
+    if (error || body.length === 2) {
+      callback(error, null, null);
+    } else if (response.statusCode !== 200) {
+      callback(null, null, response.statusCode);
+  } else {
       const data = JSON.parse(body);
-      callback(null, data["0"].description)
+      callback(null, data["0"].description, response.statusCode);
     }
-    });
-}
+  });
+};
 
 module.exports = {
   fetchBreedDescription
-}
+};
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const breedName = process.argv[2];
 const { fetchBreedDescription } = require('./breedFetcher');
 
-fetchBreedDescription(breedName, (error, description) => {
+fetchBreedDescription(breedName, (error, description, statusCode) => {
   if (error) {
     console.log('Error fetch details:', error);
+  } else if (statusCode !== 200) {
+    console.log(statusCode);
   } else {
     console.log(description);
   }
-})
+});
